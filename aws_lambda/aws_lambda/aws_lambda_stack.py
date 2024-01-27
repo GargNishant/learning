0cfe89dbf6877367b1ddebe218ae5566b105a757
Nishant Garg <nishantgarg76342@gmail.com>
2024-01-27 14:03:32 +0530

    CDK (#27)

--- aws_lambda/aws_lambda/aws_lambda_stack.py
+++ aws_lambda/aws_lambda/aws_lambda_stack.py
@@ -1,7 +1,10 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_lambda as _lambda,
+    aws_sqs as sqs,
+    aws_sns as sns,
+    aws_lambda_destinations as destinations,
+    aws_sns_subscriptions as subscriptions
 )
 from constructs import Construct
 
@@ -10,10 +13,16 @@ class AwsLambdaStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        destination_queue = sqs.Queue(self, "DestinationQueue")
+        trigger_sns = sns.Topic(self, "TriggerSns")
+        my_lambda = _lambda.Function(
+            self, 'MyLambda',
+            runtime=_lambda.Runtime.PYTHON_3_11,
+            code=_lambda.Code.from_asset('lambda_code'),
+            handler='lambda_1.handler',
+            on_success=destinations.SqsDestination(destination_queue),
+            reserved_concurrent_executions=3
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "AwsLambdaQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        my_lambda.add_alias("Live", provisioned_concurrent_executions=2)
+        trigger_sns.add_subscription(subscriptions.LambdaSubscription(my_lambda))
